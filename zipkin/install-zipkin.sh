6512a1bb1ba0a2aabd21ce8758bc0a64800d23cb
JunjunZhu <junjun.zhu@outlook.com>
2018-04-18 00:06:15 +0800

    add zipkin and fixed some bugger

--- zipkin/install-zipkin.sh
+++
@@ -1,9 +0,0 @@
-##
-## Spring boot 2.0 don't support the latest version 2.7.1 zipkin-server.jar，So you must install zipkin in yourself.
-##
-docker search zipkin
-docker pull openzipkin/zipkin
-
-## cd the git project zipkin path and run the below command.
-docker-compose up -d
-
