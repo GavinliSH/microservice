5cae0b01b36cbd55dc937e261731dd8fc11069fa
Junjun Zhu <zjy697189@163.com>
2018-04-17 12:22:38 +0800

    Update install-mysql.sh

--- zipkin/install-mysql.sh
+++ zipkin/install-mysql.sh
@@ -3,4 +3,4 @@
 ##
 docker search mysql
 docker pull mysql
-docker run --name zipkin -p 3306:3306  -e MYSQL_ROOT_PASSWORD=123456 -d mysql
+docker run --name mysql -p 3306:3306  -e MYSQL_ROOT_PASSWORD=123456 -d mysql
