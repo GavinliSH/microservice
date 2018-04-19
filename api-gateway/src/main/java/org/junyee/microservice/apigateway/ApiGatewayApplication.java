d76653836778e24aab53c3d8cd02eb84c23d0b08
JunjunZhu <junjun.zhu@outlook.com>
2018-04-19 23:27:46 +0800

    add api gateway

--- api-gateway/src/main/java/org/junyee/microservice/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/junyee/microservice/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,16 @@ package org.junyee.microservice.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableCircuitBreaker
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
